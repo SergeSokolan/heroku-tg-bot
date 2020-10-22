7a2b77bf5a20e19d91a18f9f8a64fd2b0317eb50
SokolanSS <sokolanss@sibintek.ru>
2020-10-22 16:23:51 +0300

    banecs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
+const banecs = require('./banecs.js');
 
-const TOKEN = '1361184278:AAE8cvVhSWWjSEyBt-huX2cbbSus5SQCi4k';
+const token = '1361184278:AAE8cvVhSWWjSEyBt-huX2cbbSus5SQCi4k';
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 
 const answersOnFour = [
   'Семь!',
@@ -11,16 +12,8 @@ const answersOnFour = [
   'Может, всё-таки семь?',
 ];
 
-const answerOnFour = () =>
-  answersOnFour[Math.floor(Math.random() * answersOnFour.length)];
-
 bot.onText(/\/start/, function (msg) {
   bot.sendMessage(msg.chat.id, 'Дарованьки, пидоры!');
-});
-
-bot.onText(/.*(?:4|.*7)/, (msg) => {
-  Boolean(match[0].split('').filter((word) => word === '7') != '7') &&
-    bot.sendMessage(msg.chat.id, '7');
 });
 
 bot.onText(/.*(?:4|.*7)/, (msg, match) => {
@@ -30,7 +23,10 @@ bot.onText(/.*(?:4|.*7)/, (msg, match) => {
 
 bot.onText(/(?:^|[^а-яА-ЯёЁ])(.*(?:четыре|.*семь))/, (msg, match) => {
   Boolean(match[0].split(' ').filter((word) => word === 'семь') != 'семь') &&
-    bot.sendMessage(msg.chat.id, answerOnFour());
+    bot.sendMessage(
+      msg.chat.id,
+      answersOnFour[Math.floor(Math.random() * answersOnFour.length)]
+    );
 });
 
 bot.onText(/.*(?:4.*7)/, (msg) => {
@@ -47,4 +43,11 @@ bot.onText(/(?:^|[^а-яА-ЯёЁ])(.*(?:ОБДбот.*как.*дела))/, (msg
 
 bot.onText(/(?:^|[^а-яА-ЯёЁ])(.*(?:ОБДбот.*говно))/, (msg) => {
   bot.sendMessage(msg.chat.id, 'Я ни при чем, меня Сергуня делал!');
+});
+
+bot.onText(/(?:^|[^а-яА-ЯёЁ])(.*(?:ОБДбот.*расскажи.*анекдот))/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    banecs[Math.floor(Math.random() * banecs.length)]
+  );
 });
